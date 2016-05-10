2366b9a26a92c8cbe79ea4f88ceae102dd15b69a
garyanewsome <garyanewsome@gmail.com>
2016-05-10 10:38:31 -0400

    web server updates, added background image, general little things

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+
 require('react-infinite-carousel')
 
-var content = document.getElementById('content'),
+var carousel = document.getElementById('carousel'),
     backgroundRenderer = function(index){
-	    return <img src={"backgroundImage.jpg"} width: '100%', height: '100%'/>
+	    return <img src={"backgroundImage.png"} width: '100%', height: '100%'/>
     },
     itemRenderer = function(index, progress){
     	return <div>{"Item number " + index} </div>
